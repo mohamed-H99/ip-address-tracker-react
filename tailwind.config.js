8f9f72d231364bbfba6f97f7f6ff480a428eda9b
ahmdbhgat4 <ahmdbhgat4@gmail.com>
2021-10-08 20:15:47 +0200

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,36 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+module.exports = {
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    screens: {
+      sm: "375px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      black: "#000",
+      white: "#fff",
+      gray: {
+        DEFAULT: "#969696",
+        dark: "#2b2b2b",
+      },
+    },
+    extend: {
+      fontFamily: {
+        sans: ["Rubik", ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+  corePlugins: {
+    fontFamily: false,
+  },
+};
